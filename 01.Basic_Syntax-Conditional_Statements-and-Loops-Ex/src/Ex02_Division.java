55ad6eef37e4b413a12033daa53d00510e8a1689
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-01-07 00:03:21 +0200

    added ex Basic syntax, conditional statements and loops"

---
+++ 01.Basic_Syntax-Conditional_Statements-and-Loops-Ex/src/Ex02_Division.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex02_Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        String output = "";
+
+        if (num % 10 == 0) {
+            output = "The number is divisible by 10";
+        } else if (num % 7 == 0) {
+            output = "The number is divisible by 7";
+        } else if (num % 6 == 0) {
+            output = "The number is divisible by 6";
+        } else if (num % 3 == 0) {
+            output = "The number is divisible by 3";
+        } else if (num % 2 == 0) {
+            output = "The number is divisible by 2";
+        } else {
+            output = "Not divisible";
+        }
+
+        System.out.println(output);
+    }
+}
