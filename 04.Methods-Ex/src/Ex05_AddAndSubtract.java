defaa6b4ffbc31caa730764bb27630009d6a49e7
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-01-15 23:23:06 +0200

    added Ex05_AddAndSubtract

---
+++ 04.Methods-Ex/src/Ex05_AddAndSubtract.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex05_AddAndSubtract {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int num3 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(subtract(sum(num1, num2), num3));
+    }
+
+    static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+}
