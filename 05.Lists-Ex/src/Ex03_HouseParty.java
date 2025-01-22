407453f71166f8e19d1cc24141046afadb7d4534
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-01-22 23:51:52 +0200

    added Ex03_HouseParty

---
+++ 05.Lists-Ex/src/Ex03_HouseParty.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex03_HouseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<String> guests = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            String name = input[0];
+
+            if (!input[2].equals("not")) {
+                if (guests.contains(name)) {
+                    System.out.printf("%s is already in the list!", name);
+                    continue;
+                }
+
+                guests.add(name);
+            } else {
+                if (!guests.contains(name)) {
+                    System.out.printf("%s is not in the list!", name);
+                    continue;
+                }
+
+                guests.removeIf(guest -> guests.contains(name));
+            }
+        }
+
+        guests.forEach(System.out::println);
+    }
+}
