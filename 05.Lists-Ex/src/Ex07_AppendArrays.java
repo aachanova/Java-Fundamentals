890734d1f0d3b3b663c1f203e0fa24a3a2bfa8bc
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-01-23 23:31:19 +0200

    added Ex07_AppendArrays

---
+++ 05.Lists-Ex/src/Ex07_AppendArrays.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Ex07_AppendArrays {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> numbers= Arrays.stream((scanner.nextLine().split("\\|"))).collect(Collectors.toList());
+        Collections.reverse(numbers);
+
+        StringBuilder output= new StringBuilder();
+
+        for (String number : numbers) {
+            output.append(number).append(" ");
+        }
+        System.out.println(output.toString().trim().replaceAll("\\s+"," "));
+    }
+}
