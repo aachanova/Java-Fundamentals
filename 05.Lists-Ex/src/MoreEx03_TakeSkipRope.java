3ef778e29638b01e01b05e31b3e457cd3a5561c0
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-01-26 21:57:09 +0200

    added MoreEx04_MixedUpLists

--- 05.Lists-Ex/src/MoreEx03_TakeSkipRope.java
+++ 05.Lists-Ex/src/MoreEx03_TakeSkipRope.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class MoreEx03_TakeSkipRope {
     public static void main(String[] args) {
