8d268fddb96866a822fd29d910c3f5d2f10b1986
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-02-03 00:27:55 +0200

    added changes to Ex05_VehicleCatalogue

--- 06.ObjectsAndClasses-Ex/src/Ex05_VehicleCatalogue/Main.java
+++ 06.ObjectsAndClasses-Ex/src/Ex05_VehicleCatalogue/Main.java
@@ -2,6 +2,7 @@ package Ex05_VehicleCatalogue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.Scanner;
 
 public class Main {
@@ -11,45 +12,45 @@ public class Main {
         String input = scanner.nextLine();
         List<Vehicle> vehicles = new ArrayList<>();
 
-        // Четене на вход за превозни средства
         while (!input.equals("End")) {
             String[] info = input.split("\\s+");
             vehicles.add(new Vehicle(info[0], info[1], info[2], Integer.parseInt(info[3])));
+
             input = scanner.nextLine();
         }
 
-        // Четене на запитвания за модели
         String model = scanner.nextLine();
+
         while (!model.equals("Close the Catalogue")) {
             final String finalModel = model;
             vehicles.stream()
-                    .filter(v -> v.getModel().equals(finalModel))  // филтрираме по модел
-                    .forEach(Vehicle::printData);  // извикваме printData без параметър
+                    .filter(v -> v.getModel().equals(finalModel))
+                    .forEach(v -> v.printData());
 
             model = scanner.nextLine();
         }
+
+        System.out.printf("Cars have average horsepower of: %.2f.%n", getCarAverageHorsepower(vehicles));
+        System.out.printf("Trucks have average horsepower of: %.2f.", getTruckAverageHorsepower(vehicles));
     }
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        String input = scanner.nextLine();
-//        List<Vehicle> vehicles = new ArrayList<>();
-//
-//        while (!input.equals("End")) {
-//            String[] info = scanner.nextLine().split("\\s+");
-//            vehicles.add(new Vehicle(info[0], info[1], info[2], Integer.parseInt(info[3])));
-//
-//            input = scanner.nextLine();
-//        }
-//
-//        String model = scanner.nextLine();
-//
-//        while (!model.equals("Close the Catalogue")) {
-//            vehicles.stream()
-//                    .filter(v -> v.getModel().equals(model))
-//                    .forEach(v -> v.printData(model));
-//
-//            model = scanner.nextLine();
-//        }
-//    }
+
+    private static double getCarAverageHorsepower(List<Vehicle> vehicles) {
+        double avgCarHorsepower = vehicles.stream()
+                .filter(v -> v.getType().equals("car"))
+                .mapToInt(Vehicle::getHorsepower)
+                .average()
+                .orElse(0.00);
+
+        return avgCarHorsepower;
+    }
+
+    private static double getTruckAverageHorsepower(List<Vehicle> vehicles) {
+        double avgTruckHorsepower = vehicles.stream()
+                .filter(v -> v.getType().equals("truck"))
+                .mapToInt(Vehicle::getHorsepower)
+                .average()
+                .orElse(0.00);
+
+        return avgTruckHorsepower;
+    }
 }
