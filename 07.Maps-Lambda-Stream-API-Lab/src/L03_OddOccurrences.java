668e47a8ccf4524fa124afeddf2e4735bbae7171
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2025-02-04 23:35:05 +0200

    added L03_OddOccurrences

---
+++ 07.Maps-Lambda-Stream-API-Lab/src/L03_OddOccurrences.java
@@ -0,0 +1,20 @@
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class L03_OddOccurrences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().toLowerCase().split("\\s+");
+
+        Map<String, Integer> words = IntStream.range(0, input.length).boxed()
+                .collect(Collectors.toMap(i -> input[i], i -> 1, Integer::sum, LinkedHashMap::new));
+        List<String> output = words.entrySet().stream().filter(entry -> entry.getValue() % 2 == 1)
+                .map(Map.Entry::getKey).collect(Collectors.toList());
+
+        System.out.println(String.join(", ", output));
+    }
+}
